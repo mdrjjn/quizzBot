ea2c1edfca4e84082993be92d3f79dbe4f0e8803
Max Drojjin <max.drojjin@gmail.com>
2017-09-26 10:43:46 -0400

    Initial bot structure added

---
+++ src/QuizBot.java
@@ -0,0 +1,41 @@
+/**
+ * Created by drmax on 9/26/17.
+ */
+
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class QuizBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(message_text);
+            try {
+                sendMessage(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "QuizzBot_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "423712729:AAHRy3okTBfx0BfxTX5E7hsrcYyXzyEZpn4";
+    }
+}
